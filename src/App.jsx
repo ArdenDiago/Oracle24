72f018157165e17b7a1eb7a9e4d557fd4722613b
Arden Diago <diagoarden@gmail.com>
2024-04-29 12:17:23 +0530

    removing some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import Background from "./Component/Background/Background";
 import LiveBG from "./Component/LiveBG/LiveBG";
 import Welcome from "./Component/Welcome/Welcome";
-import Test from "./Component/test/test";
 import Text from "./Component/Text/Text";
 
 function App() {
   return (
     <>
       <LiveBG />
-      {/* <Background /> */}
       <Text />
-
       <Welcome />
-      {/* <Test /> */}
     </>
   );
 }
