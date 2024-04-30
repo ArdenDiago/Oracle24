d19a4bd55b54d9ca9147cdae86553c802b7cd50b
Arden Diago <diagoarden@gmail.com>
2024-04-30 19:45:44 +0530

    mytesst

--- src/Component/IndexElements/Background/Background2.jsx
+++ src/Component/IndexElements/Background/Background2.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import "./Background2.css";
 
 export default function Background() {
+  useEffect(() => {
+    document.addEventListener("keydown", detectKeyDown, true);
+  }, []);
+  const detectKeyDown = (e) => {
+      if (e.key === "ArrowDown") {
+        console.log('Down');
+      }
+  };
   return (
     <>
       <div className="header-nav">
