e7862becfd3925c08dbc77ffc9d3babe11e79f31
Arden Diago <diagoarden@gmail.com>
2024-05-01 11:13:57 +0530

    half done terminal

--- src/Component/IndexElements/Background/Line/Line.jsx
+++ src/Component/IndexElements/Background/Line/Line.jsx
@@ -1,7 +1,8 @@
-export default function Line() {
+import './Line.css';
+export default function Line({intruction}) {
   return (
     <>
-      <h1>Pegasus@Localhost:~$</h1> <p>{command}</p>
+      <h1>Pegasus@Localhost:~$</h1> <p>{intruction}</p>
       <div className="text"></div>
     </>
   );
--- src/Component/IndexElements/Background/Man/Name.jsx
+++ src/Component/IndexElements/Background/Man/Name.jsx
@@ -1,17 +1,26 @@
-import "./Name.css"
-export default function Name({x,y}){
-    return(<>
-    <div >
-        
-        <div className="Name">
-            {x}
-        </div>
-        <div className="ls">
-            {y}
-        </div>
+import "./Name.css";
 
-    </div>
+export default function Name({ x, y }) {
+  return (
+    <>
+      <div className="background-width">
+        <div className="header-name">
+            <div className="header-command-name">
+                {x}
+            </div>
+            <div className="header-command-name">
+                <p>INTRA DEPARTMENT TECHNO-CULTURAL FEST</p>
+            </div>
+
+            <div className="header-command-name left">
+                {x}
+            </div>
+        </div>
+        <div>
+          <div className="Name">{x}</div>
+          <div className="ls">{y}</div>
+        </div>
+      </div>
     </>
-
-    );
+  );
 }
